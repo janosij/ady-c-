ea8ca040477f6815d11abacc3390b18630ea9e36
janosij <janosij12@gmail.com>
2018-02-03 22:58:19 +0100

    #2: Az elágozásban már változót is használok

--- ady1/02Recept/Program.cs
+++ ady1/02Recept/Program.cs
@@ -15,6 +15,7 @@ namespace _02Recept
         {
             //feltételt vizsgálunk, ha péntek van, akkor hal, ha nem, akkor marha pörkölt
             //vedd észre, hogy az if sorát nem zárjuk ;-vel!!!
+
             if (DateTime.Today.DayOfWeek==DayOfWeek.Friday) //lekérdezzük a dátumot, összehasonlítjuk a péntekkel
             {
                 Console.WriteLine("Halas kaja lesz"); //ha igaz a feltétel, akkor ez hajtódik végre
@@ -27,6 +28,31 @@ namespace _02Recept
                 Console.WriteLine("Paprika");
 
             }
+            Console.WriteLine(); //hagyjon ki egy üres sort
+
+            /*Lehet változóval is! Ilyenkor létre kell hozni változóket, amelyek közül az egyik a valódi 
+            aktuális napot tartalmazza, a másik, hogy ez a hét melyik napja, a harmadik meg azt, 
+            hogy melyik nap szeretnénk halat enni*/
+
+            var aktualis_nap = DateTime.Today; //milyen nap van ma
+            
+            var a_het_melyik_napja = aktualis_nap.DayOfWeek; //ez a hét melyik napja
+
+            var halas_nap = DayOfWeek.Friday; //ezen a napon zabálunk halat
+
+            if (a_het_melyik_napja==halas_nap)
+            {
+                Console.WriteLine("Halas kaja lesz"); //ha igaz a feltétel, akkor ez hajtódik végre
+            }
+            else //ez után lesz a hamis ág
+            {
+                Console.WriteLine("Marhahús"); //ha nem péntek van, akkor ezeket írja ki
+                Console.WriteLine("Vöröshagyma");
+                Console.WriteLine("Olaj");
+                Console.WriteLine("Paprika");
+
+            }
+
             Console.ReadKey(); //itt meg vár egy billentyű lenyomására
         }
     }
