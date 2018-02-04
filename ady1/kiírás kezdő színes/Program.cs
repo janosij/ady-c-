f984f2ad3886128ca7734e1fe9173328bfe50c15
janosij <janosij12@gmail.com>
2018-02-04 21:53:31 +0100

    #1: Kiírás II. rész

---
+++ ady1/kiírás kezdő színes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Kezdo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name; //változó létrehozása
+            Console.WriteLine("Szia! Add meg a neved:"); //kiírás a képernyőre
+            name = Console.ReadLine(); //beolvassuk a felhasználó által beírt értéket az előbb létrehozott változóba
+
+            Console.WriteLine("Szia kedves " + name); //kiírjuk az eredményt
+
+            Console.SetCursorPosition(20, 20); //arrébb tesszük a kurzort
+            Console.BackgroundColor = ConsoleColor.DarkGreen; //háttérszínt adunk
+            Console.ForegroundColor = ConsoleColor.Yellow; //betűszínt adunk
+            Console.WriteLine("Szia kedves {0}", name); //kiírjuk újra, de más módszerrel!
+            Console.ResetColor(); //visszaállunk eredeti színre
+            Console.SetCursorPosition(25, 25);
+
+            Console.WriteLine(name);
+
+
+            Console.ReadKey(); //addig marad a képernyő, amíg nem nyomunk meg egy billentyűt
+        }
+    }
+}
